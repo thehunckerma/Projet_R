07db8138a15eba786ab7b7829f4d7ffb377c6672
SIDATE El Mahdi <thehunckerma@hotmail.com>
2020-06-20 21:10:47 +0000

    Add Chi-square & Cramer'v tests for all var pairs

--- hypothesis.R
+++ hypothesis.R
@@ -2,66 +2,162 @@ source("helpers.R")
 
 packages <- c( # List of this script's dependencies
   "readxl",
+  "dplyr",
   "questionr",
   "rlist",
   "psy",
   "data.table",
-  "mokken"
+  "mokken",
+  "DescTools"
 )
 RequirePackages(packages)
 
 
 df1 <- read_excel("data/dataset_clean_1.xlsx")
-attach(df1)
-tbl=as.data.table(df1)
-a=list(
-  list(I1,P5), #Perte de connexion ou debit faible / l'assiduité
-  list(J,P5), #nombre de sceance par semaine / l'assurance desprofs de l'assiduité des eleves
-  list(K1,P4), #nombre de devoir / l'assurance desprofs de l'assiduité des eleves
-  list(K1,P5), #Les quiz / l'assiduité
-  list(Q1,P4), #Les quiz / l'assurance des profs de l'assiduité des eleves
-  list(Q1,P5), #Les quiz / l'assiduité
-  list(Q2,P4), #l'appel / l'assurance des profs de l'assiduité des eleves
-  list(Q2,P5), #l'appel / l'assiduité
-  list(Q3,P4), #Les Qs durants le cours / l'assurance desprofs de l'assiduité des eleves
-  list(Q3,P5), #Les Qs durants le cours / l'assiduité
-  list(N,R4), #nombre de sceance par semaine / le stress
-  list(A,P5) #l'age / l'assiduité
+
+
+# definition of test functions
+CramerV <- function(x) {
+
+}
+kendallTauB <- function(x) {
+  KendallTauB(table(factor(x[[1]], ordered = TRUE), factor(x[[2]], ordered = TRUE)), conf.level = 0.95)
+}
+
+df1_colnames <- colnames(df1) # List of dataframe column names
+counter_ <- 0 # Counts the number Chi-square tests with p-value < 0.05
+counter <- 0 # Counts the total number of the Chi-square tests
+chi2_df <- data.frame(
+  row1 = numeric(),
+  row2 = numeric(),
+  name1 = character(),
+  name2 = character(),
+  p_value = numeric()
+) # Dataframe that stores data about the variables that got a p-value < 0.05
+
+# Perform Chi-square test on every pair of variables from the dataframe
+for (row in 1:ncol(df1)) {
+  for (row_ in row:ncol(df1)) {
+    # Convert to ordinal variable
+    x <- df1[row] %>%
+      as.matrix() %>%
+      as.table() %>%
+      factor()
+    y <- df1[row_] %>%
+      as.matrix() %>%
+      as.table() %>%
+      factor()
+
+    if (length(summary(x)) < 2 | row == row_) next
+
+    p_value <- chisq.test(table(x, y), simulate.p.value = TRUE)$p.value # Chi-square test of the selected variables
+
+    if (p_value <= 0.05) {
+      print(paste("There is a correlation between", df1_colnames[row], "and", df1_colnames[row_]))
+      writeLines(paste("p-value =", p_value, "\n"))
+
+      chi2_df <- rbind(chi2_df, list(
+        row1 = row,
+        row2 = row_,
+        name1 = df1_colnames[row],
+        name2 = df1_colnames[row_],
+        p_value = p_value
+      )) # Store the information in the dataframe for later use
+
+      counter_ <- counter_ + 1
+    }
+    counter <- counter + 1
+  }
+}
+print(paste("There are", counter_, "correlations out of", counter))
+View(chi2_df)
+
+# Dataframes that stores data about the variables depending on their cramer's v coeff value
+cramerv_df_low <- data.frame(
+  row1 = numeric(),
+  row2 = numeric(),
+  name1 = character(),
+  name2 = character(),
+  p_value = numeric(),
+  cramer_v = numeric()
+)
+cramerv_df_moderate <- data.frame(
+  row1 = numeric(),
+  row2 = numeric(),
+  name1 = character(),
+  name2 = character(),
+  p_value = numeric(),
+  cramer_v = numeric()
+)
+cramerv_df_high <- data.frame(
+  row1 = numeric(),
+  row2 = numeric(),
+  name1 = character(),
+  name2 = character(),
+  p_value = numeric(),
+  cramer_v = numeric()
 )
 
+# Calculate Cramer's V coeff for every pair of variables that passed the Chi-square test
+# >.5 high association
+# .3 to .5 moderate association
+# .1 to .3 low association
+# 0 to .1 little if any association
 
-#definition of test functions
-CramerV= function(x){
-  cramer.v(table(factor(x[[1]],ordered=TRUE),factor(x[[2]],ordered=TRUE)))^2
+for (row in 1:nrow(chi2_df)) {
+  r <- chi2_df$row1[row]
+  r_ <- chi2_df$row2[row]
+  name1 <- chi2_df$name1[row]
+  name2 <- chi2_df$name2[row]
+  p_value <- chi2_df$p_value[row]
+
+  x <- df1[r] %>%
+    as.matrix() %>%
+    as.table() %>%
+    factor()
+  y <- df1[r_] %>%
+    as.matrix() %>%
+    as.table() %>%
+    factor()
+
+  if (length(summary(x)) < 2 | length(summary(y)) < 2) next
+
+  c <- suppressWarnings(cramer.v(table(x, y))) # Cramer's v coeff of the selected variables
+
+  # Seperate the data depending on the coefficient's value
+  if (c >= 0.5) {
+    print(paste("There is a high correlation between", df1_colnames[r], "and", df1_colnames[r_]))
+    cramerv_df_high <- rbind(cramerv_df_high, list(
+      row1 = r,
+      row2 = r_,
+      name1 = name1,
+      name2 = name2,
+      p_value = p_value,
+      cramer_v = c
+    )) # Store the information in the dataframe for later use
+  } else if (c >= 0.3 & c < 0.5) {
+    print(paste("There is a moderate correlation between", df1_colnames[r], "and", df1_colnames[r_]))
+    cramerv_df_moderate <- rbind(cramerv_df_moderate, list(
+      row1 = r,
+      row2 = r_,
+      name1 = name1,
+      name2 = name2,
+      p_value = p_value,
+      cramer_v = c
+    )) # Store the information in the dataframe for later use
+  } else if (c >= 0.1 & c < 0.3) {
+    print(paste("There is a low correlation between", df1_colnames[r], "and", df1_colnames[r_]))
+    cramerv_df_low <- rbind(cramerv_df_low, list(
+      row1 = r,
+      row2 = r_,
+      name1 = name1,
+      name2 = name2,
+      p_value = p_value,
+      cramer_v = c
+    )) # Store the information in the dataframe for later use
+  }
+  writeLines(paste("Cramer's v coeff =", c, "\n"))
 }
-PearsonKhi2= function(x){
-  chisq.test(table(factor(x[[1]],ordered=TRUE),factor(x[[2]],ordered=TRUE)),simulate.p.value = TRUE)
-}
-kendallTauB=function(x){
-  KendallTauB(table(factor(x[[1]],ordered=TRUE),factor(x[[2]],ordered=TRUE)), conf.level=0.95)
-}
-
-#execution of the tests
-pKhi2=lapply(a,PearsonKhi2) #applying Khi2 test to all elements of the list of "a" --lists of questions--
-
-depPValue=list.filter(a,PearsonKhi2(a[[.i]])$p.value < 0.05,simulate.p.value = TRUE) #filtering list and keeping only elements with p-value < 0.05
-
-cramer=lapply(depPValue,CramerV) #applying cramerV test to the remaining elements after filter
-
-kendallTB=lapply(depPValue,kendallTauB) #applying kendallTB test to the remaining elements after filter
-
-alphaCronbach=cronbach(tbl[,1:length(tbl)]) #applying the Conbach alpha test on the dataframe (as a data table) to check data reliability based on the alpha value
-
-#applying the mokken scaling, Conbach alpha test, the Guttman's lambda test, on the dataframe to check data reliability
-check.reliability(as.data.frame(df1), MS = TRUE, alpha = TRUE, lambda.2 = TRUE, LCRC = FALSE, nclass = nclass.default, irc = FALSE)
-
-
-
-
-
-# #hypothese: l'appel impacte l'assiduité
-# chi2CramereV(Q2,P5)
-# #hypothese: Les Qs durants le cours impacte l'assurance desprofs de l'assiduité des eleves
-# chi2CramereV(Q3,P4)
-# #hypothese: Les Qs durants le cours impacte l'assiduité
-# chi2CramereV(Q3,P5)
+View(cramerv_df_moderate)
+View(cramerv_df_low)
+View(cramerv_df_high)
