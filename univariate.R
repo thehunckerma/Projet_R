665b407abf4b83717292a52b435235e397dd95eb
BMC666 <jnourinho@gmail.com>
2020-06-21 12:40:18 +0100

    update

--- univariate.R
+++ univariate.R
@@ -3,30 +3,33 @@ source("helpers.R")
 packages <- c( # List of this script's dependencies
   "readxl",
   "Hmisc",
-  "moments"
+  "moments",
+  "vcd"
 )
 RequirePackages(packages) # if package is installed import it else install it and import it
+
 dataset <- read_excel("data/dataset_clean.xlsx") # Import the dataset
-attach(dataset)
+
+df1 <- read_excel("data/dataset_clean_1.xlsx") # Import the dataset
+
 dataset1 <- read_excel("data/dataset_clean_1.xlsx") # Import the dataset
-attach(dataset1)
-#Max, min, mediane, mean, 1st and 3rd quantils, variace, square distance, kurtosis, skewness, frequences
-summary(A)
-var(A)
-sd(A)
-kurtosis(A) # 
-skewness(A)
-table(A)
-hist(A)
-shapiro.test(A) 
+
+#Max, min, mediane, mean, 1st and 3rd quantils, mode, variace, square distance, kurtosis, skewness, frequences
+
+summary(dataset$A)
+Mode(dataset$A)
+var(dataset$dataset$A)
+sd(dataset$A)
+kurtosis(dataset$A) # 
+skewness(dataset$A)
+table(dataset$A)
+hist(dataset$A)
+shapiro.test(dataset$A) 
 
 p_OnlineCourses = nrow(dataset1)/nrow(dataset) # Ratio of people who had online courses 
 p_NoOnlineCourses = 1 - p_OnlineCourses # Ratio of people who didn't have online courses 
-
 # Frequency
-frequence=lapply(colnames(dataset[,2:length(dataset)]),function(x){
-    table(dataset[x])
-})
+frequence=lapply(colnames(dataset[,2:length(dataset)]),function(x){table(dataset[x]))
 # Pourcentage
 pourcentage=lapply(lapply(frequence,"*",100),"/",nrow(dataset))
 
