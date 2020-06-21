6d3381cee1fee2173f3205982efe6bc1243b7679
BMC666 <jnourinho@gmail.com>
2020-06-21 02:34:46 +0100

    Univariate Analysis

--- univariate.R
+++ univariate.R
@@ -9,7 +9,8 @@ RequirePackages(packages) # if package is installed import it else install it an
 
 dataset <- read_excel("data/dataset_clean.xlsx") # Import the dataset
 attach(dataset)
-
+dataset1 <- read_excel("data/dataset_clean_1.xlsx") # Import the dataset
+attach(dataset1)
 #Max, min, mediane, mean, 1st and 3rd quantils, variace, square distance, kurtosis, skewness, frequences
 summary(A)
 var(A)
@@ -20,14 +21,20 @@ table(A)
 hist(A)
 shapiro.test(A) 
 
-#frequence
-frequence=lapply(colnames(dataset[,2:length(dataset)]),function(x)  table(dataset[x]))
-names(frequence)=
+p_OnlineCourses = nrow(dataset1)/nrow(dataset) # Ratio of people who had online courses 
+p_NoOnlineCourses = 1 - p_OnlineCourses # Ratio of people who didn't have online courses 
+
+# Frequency
+frequence=lapply(colnames(dataset[,2:length(dataset)]),function(x){
+    table(dataset[x])
+})
+# Pourcentage
+pourcentage=lapply(lapply(frequence,"*",100),"/",nrow(dataset))
 
 
-hist.data.frame(dataset[,1:5])
-hist.data.frame(dataset[,15:26])
-hist.data.frame(dataset[,27:37])
-hist.data.frame(dataset[,38:48])
-hist.data.frame(dataset[,49:59])
-hist.data.frame(dataset[,60])
+hist.data.frame(dataset[,2:12])
+hist.data.frame(dataset[,13:23])
+hist.data.frame(dataset[,24:34])
+hist.data.frame(dataset[,35:45])
+hist.data.frame(dataset[,46:56])
+hist.data.frame(dataset[,57:60])
