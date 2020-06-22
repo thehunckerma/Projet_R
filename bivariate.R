b6df895b22c6737317835c61078c60212305ec26
Abdennour Jebbar <35144705+Shadow13X@users.noreply.github.com>
2020-06-22 20:55:57 +0100

    Exact Wilcoxon test, hists of comparison

--- bivariate.R
+++ bivariate.R
@@ -2,9 +2,9 @@ source("helpers.R")
 
 packages <- c( # List of this script's dependencies
   "readxl",
-  "stats", #wilcox.test
   "purrr", #set_names
-  "ggplot2" #geom_histograms
+  "ggplot2", #geom_histograms
+  "exactRankTests" #wilcox_test
 )
 RequirePackages(packages)
 
@@ -26,7 +26,7 @@ women=women[1:min(nrow(men),nrow(women)),]
 # if there is a difference between the results based o the p-value
 
 messages=unlist(lapply(names(c(map1[1],map1[3:length(map1)])),function(x){
-   p_value=wilcox.test(unlist(men[x]),unlist(women[x]),paired=TRUE)$p.value
+   p_value=wilcox.exact(unlist(men[x]),unlist(women[x]),paired=FALSE)$p.value
    if(p_value>=0.05) paste("There is no significant difference in the", map1[x], "between men and women")
    else paste("There is a significant difference in the", map1[x], "between men and women")
 }))
@@ -39,13 +39,12 @@ T=set_names(T)
 twoSamples=lapply(T,function(x) {
       ggplot(men, aes(x = .data[[x]]) ) + geom_histogram(fill="blue",alpha=0.3,binwidth = NULL) + geom_histogram(data=women,fill="red",binwidth = NULL,alpha=0.3)
 })
-twoSamples[1]
 
 #generating grid plots and saving them to img folder
-ggsave("img/twoSamples1.png",do.call(grid.arrange, twoSamples[2:13]))
-ggsave("img/twoSamples2.png",do.call(grid.arrange, twoSamples[14:25]))
-ggsave("img/twoSamples3.png",do.call(grid.arrange, twoSamples[26:37]))
-ggsave("img/twoSamples4.png",do.call(grid.arrange, twoSamples[38:49]))
-ggsave("img/twoSamples5.png",do.call(grid.arrange, twoSamples[50:60]))
+ggsave("img/twoSamples1.png",do.call(grid.arrange, twoSamples[1:12]))
+ggsave("img/twoSamples2.png",do.call(grid.arrange, twoSamples[13:24]))
+ggsave("img/twoSamples3.png",do.call(grid.arrange, twoSamples[25:36]))
+ggsave("img/twoSamples4.png",do.call(grid.arrange, twoSamples[37:48]))
+ggsave("img/twoSamples5.png",do.call(grid.arrange, twoSamples[49:60]))
 
 #----------correlations--------------------------------------------------------
